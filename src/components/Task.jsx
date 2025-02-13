062a8d3f54f579ed30b96f129b20cb1f8596871b
brunoandersons <brunoanderson.br@gmail.com>
2025-02-13 00:27:55 -0300

    Correção na tela geradora de tarefas.

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,6 +2,7 @@ import { TrashIcon } from "lucide-react";
 import { ChevronRightIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
+import { CheckIcon } from "lucide-react";
 
 function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   const navigate = useNavigate();
@@ -19,10 +20,11 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => onTaskClick(task.id)}
-            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+            className={`bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md ${
               task.isCompleted && "line-through"
             }`}
           >
+            {task.isCompleted && <CheckIcon />}
             {task.title}
           </button>
           <Button onClick={() => onSeeDetailsClick(task)}>
